ca31ea5a96d1674c6a6cb59d24895ce950bd2f46
Personwritep <89448434+personwritep@users.noreply.github.com>
2026-01-14 22:48:31 +0900

    Update Page_Sample_Blog.user.js

--- Page_Sample_Blog.user.js
+++ Page_Sample_Blog.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Page Sample Blog 🔵
 // @namespace        http://tampermonkey.net/
-// @version        1.4
+// @version        1.5
 // @description        ブログページサンプルのプレゼンテーション「Ctrl+F2」
 // @author        Ameba Blog User
 // @match        https://ameblo.jp/*
@@ -79,11 +79,11 @@ function main(){
 
 
     // アメブロヘッダー ログインユーザー表示
-    let login_Avatar=document.querySelector('._2KFZbuvC >img');
+    let login_Avatar=document.querySelector('._1fy8GCTR >img');
     if(login_Avatar){
         login_Avatar.setAttribute('srcset', Avatar); } // ログインユーザー アイコン
 
-    let login_name=document.querySelector('p._w6MHwCAy');
+    let login_name=document.querySelector('p._2j4IoOdR');
     if(login_name){
         login_name.textContent='Login User'; } // ログインユーザー名
 
@@ -237,5 +237,3 @@ function main(){
 
 
 } // main()
-
-
